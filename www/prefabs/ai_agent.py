21431a4beacd64c615925964a4e73471562c8d57
Nuno Machado <nmiguelmoura@gmail.com>
2018-01-21 12:06:10 +0000

    type: refactor - simplify some python methods. docs - Add comments to python methods.

--- www/prefabs/ai_agent.py
+++ www/prefabs/ai_agent.py
@@ -1,96 +1,184 @@
-"""This class holds the code to ai gameplay decision."""
-
 import board_logic
 import random
 import sys
 
 
 class Ai_agent():
+    """This class holds the code to ai gameplay decision."""
+
+    # Get an instance of board logic class
     bl = board_logic.Board_logic()
 
     def __init__(self):
         pass
 
     def apply_move(self, board_state, move, side):
-        move_x, move_y = move
-        state_list = list(list(s) for s in board_state)
-        state_list[move_x][move_y] = side
-        return tuple(tuple(s) for s in state_list)
+        # Method to apply a move and return a new board.
+        # @board_state - game board.
+        # @ move - array of type [row, column].
+        # @side - 1 for player and -1 for CPU.
+
+        # Get row and column from move array.
+        row, column = move
+
+        # Generate a new board from the board_state given
+        new_board = list(list(s) for s in board_state)
+        new_board[row][column] = side
+        return new_board
 
     def score_line(self, line, rows):
-        minus_count = line.count(-1)
-        plus_count = line.count(1)
+        # Method to score a line. Returns 1 if the player is about to win or
+        # -1 if the CPU is about to win
+        # @line - array of values to test
+        # @ rows - the number of rows in the board.
+
+        # Count the cpu moves in the given line.
+        cpu_count = line.count(-1)
+
+        # Count the player moves in the given line.
+        player_count = line.count(1)
+
+        # Get the desired moves value a given player has to be almost winning.
         desired_count = rows - 1
 
-        if plus_count == desired_count and minus_count == 0:
+        if player_count == desired_count and cpu_count == 0:
+            # If player (user) matches the desired count and cpu has not made any
+            # move in this line, return 1. Player (user) is about to win the game.
             return 1
-        elif minus_count == desired_count and plus_count == 0:
+        elif cpu_count == desired_count and player_count == 0:
+            # If cpu matches the desired count and the player (user) has not made any
+            # move in this line, return -1. Cpu is about to win the game.
             return -1
         return 0
 
-
     def evaluate(self, board_state, rows):
+        # Method to evaluate (score) a given board.
+        # @board_state - game board.
+        # @rows - number of board rows.
+
+        # Initialize score to 0.
         score = 0
-        length = len(board_state)
 
         # rows
-        for x in range(length):
+        for x in range(rows):
+            # Score each row.
             score += self.score_line(board_state[x], rows)
 
         # columns
-        for y in range(length):
+        for y in range(rows):
+            # Score each column.
             score += self.score_line([i[y] for i in board_state], rows)
 
         # diagonals
-        score += self.score_line([board_state[i][i] for i in range(length)], rows)
-        score += self.score_line([board_state[length - 1 - i][i] for i in range(length)], rows)
+        # Score each diagonal.
+        # Diagonal top/left to bottom/right.
+        score += self.score_line([board_state[i][i] for i in range(rows)],
+                                 rows)
 
+        # Diagonal top/right to bottom/left.
+        score += self.score_line(
+            [board_state[rows - 1 - i][i] for i in range(rows)], rows)
+
+        # Return the score.
         return score
 
-
     def min_max(self, board_state, side, max_depth, rows):
-        best_score = None
-        best_score_move = None
+        # Perform minmax algorithm. This algorithm, and the methods it needs to,
+        # work takes inspiration on the one used in "Deep Learning" book
+        # from Packt Publishing.
+        # "Deep Learning", Valentino Zocca et al, 2017, Packt Publishing.
+        # @board_state - board game.
+        # @side - +1 for player or -1 for cpu.
+        # max_depth - steps after which the recursive function is stopped.
+        # rows - the number of rows in the board.
 
+        # Initialize best_score
+        best_score = None
+
+        # Initialize best_move
+        best_move = None
+
+        # Get a list of the available moves.
         moves = list(self.bl.moves_available(board_state))
+
         if not moves:
-            return 0, None
+            # If there are no moves, return 0 score and list of None.
+            return 0, [None, None]
 
         for move in moves:
+            # Loop through each available move
+
+            # Get a possible new board state, applying the given move.
             new_board_state = self.apply_move(board_state, move, side)
+
+            # Check if there is a winner in this possible state.
             winner, _ = self.bl.game_won(new_board_state)
             if winner != 0:
+                # If there is a winner, return a higher score and the move.
                 return winner * 100, move
             else:
+                # Run if there is no winner.
                 if max_depth <= 1:
+                    # If max_depth is reached, get the score of the current
+                    # possible board
                     score = self.evaluate(new_board_state, rows)
                 else:
-                    score, _ = self.min_max(new_board_state, -side, max_depth - 1, rows)
+                    # If max_depth is not reached, reapply min_max again.
+                    score, _ = self.min_max(new_board_state, -side,
+                                            max_depth - 1, rows)
+
 
                 if side > 0:
+                    # If side is 1 (player)
                     if best_score is None or score > best_score:
+                        # If score obtained in this possible move is higher than
+                        # the previous best score, make best_score equal to score
+                        # and keep the move.
                         best_score = score
-                        best_score_move = move
+                        best_move = move
                 else:
+                    # If side is -1 (cpu)
                     if best_score is None or score < best_score:
+                        # If score obtained in this possible move is lower than
+                        # the previous best score, make best_score equal to score
+                        # and keep the move.
                         best_score = score
-                        best_score_move = move
-                        # print "min_max side: %s, max_depth: %s, move: %s, score: %s" % (side, max_depth, best_score_move, best_score)
+                        best_move = move
 
-        return best_score, best_score_move
+        # Return best_score and best_move
+        return best_score, best_move
 
     def random_move(self, board_state):
+        # Method to select a random move from the list of available moves.
+        # @board_state - current game board.
+
+        # Get a list of available moves.
         moves = list(self.bl.moves_available(board_state))
-        move = random.choice(moves)
-        return {
-            'row': move[0],
-            'column': move[1]
-        }
+
+        # Return a randomly selected move.
+        return random.choice(moves)
 
     def make_move(self, board_state, type):
+        # Method to return a move from cpu.
+        # @board_state - current game board.
+        # @type - can be "ai" (for minmax decision) or "random/other" - for a
+        # random move.
+
+        # Initialize move.
+        move = None
+
         if type == "ai":
+            # Check the number of rows.
             length = len(board_state)
+
+            # Initialize variable max_depth
             max_depth = None
+
+            # Decide the max_depth value, according to the number of rows in
+            # the board. This value is important, because for 4 and 5 row games,
+            # higher max_depth values have much better results, but much bigger
+            # calculation times, making the game unplayable.
+
             if length == 3:
                 max_depth = 5
             elif length == 3:
@@ -98,11 +186,15 @@ class Ai_agent():
             else:
                 max_depth = 4
 
-            best_score, best_score_move = self.min_max(board_state, -1, max_depth, length)
-            return {
-                "row": best_score_move[0],
-                "column": best_score_move[1],
-                "best_score": best_score
-            }
+            # Get the best score and move from minmax decision.
+            score, move = self.min_max(board_state, -1, max_depth, length)
+
         else:
-            return self.random_move(board_state)
+            # Return a dictionary with the selected move.
+            move = self.random_move(board_state)
+
+        # Return a dictionary with the selected move.
+        return {
+            "row": move[0],
+            "column": move[1]
+        }
--- www/prefabs/board_logic.py
+++ www/prefabs/board_logic.py
@@ -1,22 +1,44 @@
-"""This class manages the board logic. Consider player A as +1 and B as -1."""
-
 import itertools
 
+
 class Board_logic():
+    """This class manages the board logic. Consider player A as +1 and B as -1.
+    An available move is marked as zero."""
+
     def __init__(self):
         pass
 
     def get_new_board(self, rows=3):
+        # Method to return a new board with the desired number of rows and equal
+        # number of columns.
+        # @rows - number of desired rows (and columns too).
         return [[0 for i in range(rows)] for j in range(rows)]
 
     def line_complete(self, line):
+        # Method to check if a given line is complete with same value (except 0).
+        # Returns True or False.
+        # @line - array of values.
+
+        # Check the length of the array.
         length = len(line)
+
+        # Total defaults to 0.
         total = 0
+
+        # Loop through all values.
         for i in line:
+            # Add the value to the total count.
             total += i
+
+        # Return True or False.
         return abs(total) == length
 
     def game_won(self, board):
+        # Method to check if a given game was won. Returns the winner if there
+        # is one, and the number of row, column or diagonal that lead to game
+        # won condition.
+        # @board - board to test if game was won or not.
+
         # Check for rows
         rows = len(board)
         line = None
@@ -44,14 +66,25 @@ class Board_logic():
         return 0, {}
 
     def moves_available(self, board):
+        # Method that loops through the board to check what moves are still
+        # available (value == 0). Returns a generator with all available moves.
+        # @board - board to check for available moves.
         rows = len(board)
         for i, j in itertools.product(range(rows), range(rows)):
             if board[i][j] == 0:
                 yield (i, j)
 
     def check_game_status(self, board):
+        # Method to check if the game is over. The game is over when a player
+        # wins or when there are no more available moves.
+        # @board - board to test if game was won or not.
+
+        # Check if a given player won the game.
         game_won, info = self.game_won(board)
+
+        # Check if there are moves available.
         moves_available = len(list(self.moves_available(board)))
+
         result = {
             "moves_available": moves_available,
             "winner": game_won,
@@ -60,6 +93,3 @@ class Board_logic():
         }
 
         return result
-
-# b = Board_logic()
-# print list(b.moves_available(b.get_new_board()))
